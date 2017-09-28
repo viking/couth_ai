67741da2c638861029af71afb822149a36b9d1ec
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2017-09-28 14:11:48 -0500

    Add rudimentary potential pickup analysis
    - add dry-run option to script

--- lib/couth_ai/manager.rb
+++ lib/couth_ai/manager.rb
@@ -7,28 +7,32 @@ module CouthAI
       @weekly_projections = Hash.new { |h, k| h[k] = {} }
     end
 
-    def calculate_projections!
-      week = league.current_week
-      result = Hash.new { |h, k| h[k] = [] }
-      roster.players.each do |player|
-        name = player.name["full"]
-        projs = weekly_projections(player.display_position, week)
-        proj = projs.find do |p|
-          proj_name = p.display_name
-          if player.display_position == "DEF"
-            proj_name = proj_name.split(" ").first
-          end
-          proj_name == name
+    def potential_pickups
+      roster_positions = settings.roster_positions
+      current_players = roster_positions.inject({}) do |hash, roster_position|
+        position = roster_position["position"]
+        if position != "BN"
+          hash[position] = roster.eligible_players_for(position)
         end
-
-        if proj
-          player.projected_points = calculator.projection(proj)
+        hash
+      end
+      result = []
+      free_agents.each do |free_agent|
+        # compare with players on roster
+        next if free_agent.projected_points.nil?
+        free_agent.eligible_positions.each do |position|
+          current_players[position].each do |player|
+            pts = player.projected_points
+            if pts.nil? || pts < free_agent.projected_points
+              result << { replace: player, with: free_agent }
+            end
+          end
         end
       end
+      result
     end
 
-    def optimize_roster!
-      calculate_projections!
+    def optimize_roster!(dry_run = false)
       roster_positions = settings.roster_positions
       roster_positions.each do |roster_position|
         position = roster_position["position"]
@@ -39,7 +43,9 @@ module CouthAI
         starter_ids = eligible[0, count].collect { |player| player.player_id }
         roster.set_starters_by_id(position, starter_ids)
       end
-      @yahoo.update_roster(team, roster)
+      if !dry_run
+        @yahoo.update_roster(team, roster)
+      end
     end
 
     def fantasy_game
@@ -71,7 +77,19 @@ module CouthAI
     end
 
     def roster
-      @roster ||= @yahoo.roster(team)
+      if @roster.nil?
+        @roster = @yahoo.roster(team)
+        calculate_projections!(@roster.players)
+      end
+      @roster
+    end
+
+    def free_agents
+      if @free_agents.nil?
+        @free_agents = @yahoo.players(league, { "status" => "FA" })
+        calculate_projections!(@free_agents)
+      end
+      @free_agents
     end
 
     private
@@ -81,6 +99,26 @@ module CouthAI
         @weekly_projections[position][week] = @nerd.weekly_projections(position, week)
       end
       @weekly_projections[position][week]
+    end
+
+    def calculate_projections!(players)
+      week = league.current_week
+      result = Hash.new { |h, k| h[k] = [] }
+      players.each do |player|
+        name = player.name["full"]
+        projs = weekly_projections(player.display_position, week)
+        proj = projs.find do |p|
+          proj_name = p.display_name
+          if player.display_position == "DEF"
+            proj_name = proj_name.split(" ").first
+          end
+          proj_name == name
+        end
+
+        if proj
+          player.projected_points = calculator.projection(proj)
+        end
+      end
     end
   end
 end
